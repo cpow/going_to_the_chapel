2717c8c4a7e95e80b1bebad54331b7c43af429a9
cpow <cpow85@gmail.com>
2016-03-04 12:07:02 -0500

    updated mofuggin links

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+  match 'registry' => 'high_voltage/pages#show', id: 'registry', via: :get
+  match 'home' => 'high_voltage/pages#show', id: 'home', via: :get
+  match 'our_story' => 'high_voltage/pages#show', id: 'our_story', via: :get
+  match 'the_hotel' => 'high_voltage/pages#show', id: 'the_hotel', via: :get
+  match 'the_venue' => 'high_voltage/pages#show', id: 'the_venue', via: :get
+  match 'the_ceremony' => 'high_voltage/pages#show', id: 'the_ceremony', via: :get
 end
