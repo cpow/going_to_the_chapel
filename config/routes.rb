6cdff778d6c404e18eff05359cdee5f273f744c3
cpow <cpow85@gmail.com>
2016-06-03 15:27:44 -0400

    new information for everyone

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   match 'registry' => 'high_voltage/pages#show', id: 'registry', via: :get
   match 'home' => 'high_voltage/pages#show', id: 'home', via: :get
+  match 'schedule_of_events' => 'high_voltage/pages#show', id: 'schedule_of_events', via: :get
   match 'our_story' => 'high_voltage/pages#show', id: 'our_story', via: :get
   match 'the_hotel' => 'high_voltage/pages#show', id: 'the_hotel', via: :get
   match 'the_venue' => 'high_voltage/pages#show', id: 'the_venue', via: :get
